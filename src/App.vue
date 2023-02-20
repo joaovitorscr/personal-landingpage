29f1287c99ec4bbd94f09346519d0cfb1f1807d1
João Vitor <duzt@pop-os.localdomain>
2023-02-20 17:52:58 -0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+    <div class="text-center mt-80">
+        <h1 class="text-5xl">João Vitor</h1>
+        <h2 class="text-lg mt-2">Front-end Developer</h2>
+    </div>
+    <div class="flex justify-center items-center mt-20 gap-4">
+        <a class="hover:scale-125" href="https://github.com/joaovitorscr">
+            <img class="pr-2" src="./assets/icons/github.svg" alt="GitHub Logo">
+        </a>
+        <a class="hover:scale-125" href="https://www.linkedin.com/in/joaovitorscruz/">
+            <img class="pr-2" src="./assets/icons/linkedin.svg" alt="Linkedin Logo">
+        </a>
+    </div>
+</template>
+
+<style scoped></style>
